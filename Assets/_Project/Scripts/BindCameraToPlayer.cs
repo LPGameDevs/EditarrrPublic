4830df7dea3f6fefdca8a3c2b85565dad8bdea22
Kiqa <ben.atkinson.official@gmail.com>
2023-04-09 12:41:48 +1000

    feat: camera tracking on player gameobject

---
+++ Assets/_Project/Scripts/BindCameraToPlayer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindCameraToPlayer : MonoBehaviour
+{
+    bool _playerFound = false;
+
+    void Update()
+    {
+        if (!_playerFound)
+        {
+            // find the player object
+            Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+            if (player != null)
+            {
+                _playerFound = true;
+                // set the parent of this (the camera follow target object) as a child of the player object
+                Transform cameraFollowTarget = transform.parent;
+                cameraFollowTarget.transform.SetParent(player);
+                cameraFollowTarget.localPosition = Vector3.zero;
+                // clean up this object/script
+                Destroy(gameObject);
+            }
+        }
+    }
+}
