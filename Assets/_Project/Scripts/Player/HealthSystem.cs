a67bafc2db8ab6f96853b51824cba4ecd0e6e8f1
Ben <69440665+neon-inkblast@users.noreply.github.com>
2023-04-09 18:04:28 +1000

    #11 by kiqa: Health system script (#28)

---
+++ Assets/_Project/Scripts/Player/HealthSystem.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+
+    private int _hitPoints = 0;
+    [SerializeField, Tooltip("Maximum hit points")] private int _maxHitPoints = 20;
+    [SerializeField, Tooltip("Initial hit points, limited to max hit points")] private int _startingHitPoints = 20;
+
+    private void Awake()
+    {
+        _hitPoints = Mathf.Max(_maxHitPoints, _startingHitPoints);
+    }
+
+    public event EventHandler OnDeath;
+    public event EventHandler OnHitPointsChanged;
+
+    public void TakeDamage(int amount)
+    {
+        _hitPoints -= amount;
+        OnHitPointsChanged?.Invoke(this, EventArgs.Empty);
+        _hitPoints = Mathf.Max(_hitPoints, 0);
+        if (_hitPoints == 0)
+        {
+            Die();
+        }
+    }
+
+    public void SetHitPoints(int amount)
+    {
+        _hitPoints = Mathf.Clamp(amount, 0, _maxHitPoints);
+        OnHitPointsChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalised()
+    {
+        return (float)(_hitPoints) / _maxHitPoints;
+    }
+
+    private void Die()
+    {
+        Debug.Log(gameObject.name + "has died");
+        OnDeath?.Invoke(this, EventArgs.Empty);
+    }
+}
