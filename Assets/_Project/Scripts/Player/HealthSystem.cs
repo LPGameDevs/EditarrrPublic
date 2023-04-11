f1a603968058a94c188b024a763afa3ba88e3e10
Ben <69440665+neon-inkblast@users.noreply.github.com>
2023-04-11 17:22:38 +1000

    Added optional invincibility frames to health script (#45)
    * feat: add invincibility frames to health system
    
    * feat: add value and previous value to the event args of the onHealthChanged event of health script

--- Assets/_Project/Scripts/Player/HealthSystem.cs
+++ Assets/_Project/Scripts/Player/HealthSystem.cs
@@ -5,39 +5,81 @@ namespace Player
 {
     public class HealthSystem : MonoBehaviour
     {
-
         private int _hitPoints = 0;
         [SerializeField, Tooltip("Maximum hit points")] private int _maxHitPoints = 20;
         [SerializeField, Tooltip("Initial hit points, limited to max hit points")] private int _startingHitPoints = 20;
+        [SerializeField, Tooltip("If entity becomes invincible for a short time after taking damage")] private bool _hasDamageCooldown = true;
+        [SerializeField, Range(0, 3f), Tooltip("How long entity is invincible after taking damage")] private float _damageCooldown = 0.3f;
+        private float _damageCooldownTimeRemaining = 0;
 
         private void Awake()
         {
-            _hitPoints = Mathf.Max(_maxHitPoints, _startingHitPoints);
+            _hitPoints = Mathf.Min(_maxHitPoints, _startingHitPoints);
         }
 
+        public class OnHealthChangedArgs : OnValueChangedArgs<int> { };
+
         public event EventHandler OnDeath;
-        public event EventHandler OnHitPointsChanged;
+        public event EventHandler OnInvincibleStarted;
+        public event EventHandler OnInvincibleEnded;
+        public event EventHandler<OnHealthChangedArgs> OnHitPointsChanged;
+
+        private void Update()
+        {
+            if (IsInvincible())
+            {
+                _damageCooldownTimeRemaining -= Time.deltaTime;
+                if (_damageCooldownTimeRemaining <= 0)
+                {
+                    OnInvincibleEnded?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
 
         public void TakeDamage(int amount)
         {
+            if (IsInvincible())
+            {
+                return;
+            }
+
+            int prevHitPoints = _hitPoints;
             _hitPoints -= amount;
-            OnHitPointsChanged?.Invoke(this, EventArgs.Empty);
             _hitPoints = Mathf.Max(_hitPoints, 0);
+
+            OnHitPointsChanged?.Invoke(this, new OnHealthChangedArgs { previousValue = prevHitPoints, value = _hitPoints });
+
             if (_hitPoints == 0)
             {
                 Die();
+            }
+            else if (_hasDamageCooldown)
+            {
+                _damageCooldownTimeRemaining = _damageCooldown;
+                OnInvincibleStarted?.Invoke(this, EventArgs.Empty);
             }
         }
 
         public void SetHitPoints(int amount)
         {
+            int prevHitPoints = _hitPoints;
             _hitPoints = Mathf.Clamp(amount, 0, _maxHitPoints);
-            OnHitPointsChanged?.Invoke(this, EventArgs.Empty);
+            OnHitPointsChanged?.Invoke(this, new OnHealthChangedArgs { previousValue = prevHitPoints, value = _hitPoints });
         }
 
         public float GetHealthNormalised()
         {
             return (float)(_hitPoints) / _maxHitPoints;
+        }
+
+        public bool IsDamageable()
+        {
+            return !_hasDamageCooldown || (_hasDamageCooldown && _damageCooldownTimeRemaining <= 0);
+        }
+
+        public bool IsInvincible()
+        {
+            return _hasDamageCooldown && _damageCooldownTimeRemaining > 0;
         }
 
         private void Die()
