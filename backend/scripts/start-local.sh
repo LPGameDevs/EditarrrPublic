fc09644c7f1771ad08a8b2a0866c590a450b21cd
Nathaniel Morihara <nathanielmorihara@gmail.com>
2023-07-12 13:04:42 +0000

    DynamoDB Table Setup Command

--- backend/scripts/start-local.sh
+++ backend/scripts/start-local.sh
@@ -8,13 +8,14 @@ set -eo pipefail
 # 4. Start the Nodejs lambda (npm install, npm run...)
 # 5. Make a confirmation query
 
+PORT=8000
 DDB="dynamodb"
 
 if [ ! -d ~/.aws ]; then 
     echo "Setting up mock AWS config & creds..."
     mkdir -p ~/.aws
     echo -e "[default]\nregion = eu-north-1" > ~/.aws/config
-    echo -e "[default]\naws_access_key_id = MOCK_ACCESS_KEY\naws_secret_access_key = MOCK_SECRET_KEY" > ~/.aws/credentials; 
+    echo -e "[default]\naws_access_key_id=fakeAccessKey\naws_secret_access_key=fakeSecretKey" > ~/.aws/credentials; 
 fi
 
 if docker ps -f "name=$DDB" --format '{{.Names}}' | grep -q "^$DDB$"; then
@@ -26,13 +27,59 @@ else
     else
         docker run \
             --name dynamodb \
-            -p 8000:8000 \
+            -p ${PORT}:${PORT} \
             -d amazon/dynamodb-local \
             -jar DynamoDBLocal.jar
     fi
 fi
 
-# TODO Set up the DDB table
+# TODO Set up the DDB table (if it isn't set up already)
+aws dynamodb create-table \
+    --table-name editarrr-level-storage \
+    --attribute-definitions \
+        AttributeName=pk,AttributeType=S \
+        AttributeName=sk,AttributeType=S \
+        AttributeName=levelCreatorId,AttributeType=S \
+        AttributeName=levelStatus,AttributeType=S \
+        AttributeName=levelUpdatedAt,AttributeType=N \
+    --key-schema \
+        AttributeName=pk,KeyType=HASH \
+        AttributeName=sk,KeyType=RANGE \
+    --global-secondary-indexes \
+        "[
+            {
+                \"IndexName\": \"levelCreatorId-levelUpdatedAt-index\",
+                \"KeySchema\": [{\"AttributeName\":\"levelCreatorId\",\"KeyType\":\"HASH\"},
+                                {\"AttributeName\":\"levelUpdatedAt\",\"KeyType\":\"RANGE\"}],
+                \"Projection\":{
+                    \"ProjectionType\":\"INCLUDE\",
+                    \"NonKeyAttributes\":[\"pk\",\"levelName\",\"levelStatus\"]
+                },
+                \"ProvisionedThroughput\": {
+                    \"ReadCapacityUnits\": 1,
+                    \"WriteCapacityUnits\": 1
+                }
+            },
+            {
+                \"IndexName\": \"levelStatus-levelUpdatedAt-index\",
+                \"KeySchema\": [{\"AttributeName\":\"levelStatus\",\"KeyType\":\"HASH\"},
+                                {\"AttributeName\":\"levelUpdatedAt\",\"KeyType\":\"RANGE\"}],
+                \"Projection\":{
+                    \"ProjectionType\":\"INCLUDE\",
+                    \"NonKeyAttributes\":[\"pk\",\"levelName\",\"levelCreatorId\"]
+                },
+                \"ProvisionedThroughput\": {
+                    \"ReadCapacityUnits\": 1,
+                    \"WriteCapacityUnits\": 1
+                }
+            }
+        ]" \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --endpoint-url http://localhost:${PORT}
+
+# aws dynamodb list-tables --endpoint-url http://localhost:${PORT}
+# aws dynamodb delete-table --table-name <table-name> --endpoint-url http://localhost:${PORT}
+
 
 # TODO Start the Nodejs lambda
 
