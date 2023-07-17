edf6f179506c9a8b67d367d8ffe3d93212c79fc7
Nathaniel Morihara <nathanielmorihara@gmail.com>
2023-07-17 01:12:20 +0000

    Edit set-var script to default to querying locally, and made request scripts runnable

--- backend/requests/set-vars.sh
+++ backend/requests/set-vars.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -eo pipefail
 
-APIID="ygfmolwgd9"
-REGION="eu-north-1"
+if [[ $REMOTE == "true" ]]; then
+    APIID="ygfmolwgd9"
+    REGION="eu-north-1"
 
-# If using CloudFormation:
-# APIID=$(aws cloudformation describe-stack-resource --stack-name editarrr-poc --logical-resource-id ServerlessHttpApi --query 'StackResourceDetail.PhysicalResourceId' --output text)
-# REGION=$(aws configure get region)
+    # If using CloudFormation:
+    # APIID=$(aws cloudformation describe-stack-resource --stack-name editarrr-poc --logical-resource-id ServerlessHttpApi --query 'StackResourceDetail.PhysicalResourceId' --output text)
+    # REGION=$(aws configure get region)
 
-export URL="https://$APIID.execute-api.$REGION.amazonaws.com/dev/levels"
+    export URL="https://$APIID.execute-api.$REGION.amazonaws.com/dev/levels"
+else
+    export URL="http://localhost:3000/levels"
+fi
+
+echo "Querying ${URL}..."
