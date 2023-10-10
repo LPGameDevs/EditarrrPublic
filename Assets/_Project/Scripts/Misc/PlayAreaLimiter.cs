37eda9a0a5ea2ccb9113a0d571b3643b05c6f098
Bolrik <62060123+Bolrik@users.noreply.github.com>
2023-10-10 11:16:43 +0200

    Level Expansion > Play Scene done?

--- Assets/_Project/Scripts/Misc/PlayAreaLimiter.cs
+++ Assets/_Project/Scripts/Misc/PlayAreaLimiter.cs
@@ -4,6 +4,7 @@ using Editarrr.LevelEditor;
 using UnityEngine;
 using Player;
 using Systems;
+using System;
 
 namespace Editarrr.Level
 {
@@ -19,30 +20,31 @@ namespace Editarrr.Level
         [SerializeField] PolygonCollider2D _cameraConfinementCollider;
         [SerializeField] float additionalPlayAreaOffset = 2f, additionalFallDeathTriggerOffset = -1.5f;
 
-        private void Awake()
+        private void OnEnable()
         {
-            EditorLevelSettings currentSettings = FindObjectOfType<LevelPlaySystem>().Manager.Settings;
+            LevelPlayManager.OnLevelLoading += this.LevelPlayManager_OnLevelLoading;
+        }
 
-            if (currentSettings == null)
-            {
-                Debug.Log("Current level state not found, play area limiter not initiated");
-                _areaCollider.enabled = false;
-                return;
-            }
+        private void OnDisable()
+        {
+            LevelPlayManager.OnLevelLoading -= this.LevelPlayManager_OnLevelLoading;
+        }
 
-            _areaColliderBase.size = new Vector2(currentSettings.EditorLevelScaleX, currentSettings.EditorLevelScaleY + additionalPlayAreaOffset);
-            _areaCollider.offset = new Vector2(currentSettings.EditorLevelScaleX * 0.5f, (currentSettings.EditorLevelScaleY * 0.5f) - 1f);
+        private void LevelPlayManager_OnLevelLoading(LevelState level)
+        {
+            _areaColliderBase.size = new Vector2(level.ScaleX, level.ScaleY + additionalPlayAreaOffset);
+            _areaCollider.offset = new Vector2(level.ScaleX * 0.5f, (level.ScaleY * 0.5f) - 1f);
 
-            _fallDeathTrigger.size = new Vector2(currentSettings.EditorLevelScaleX, _fallDeathTrigger.size.y);
+            _fallDeathTrigger.size = new Vector2(level.ScaleX, _fallDeathTrigger.size.y);
             _fallDeathTrigger.offset = new Vector2(_fallDeathTrigger.size.x * 0.5f, -_areaColliderBase.offset.y + additionalFallDeathTriggerOffset);
 
             //Cinemachine's camera confiner requires a polygon collider, which needs to be set up point by point. CreatePrimitive doesn't work here.
             //_cameraConfinementCollider.CreatePrimitive(4, new Vector2(currentSettings.EditorLevelScaleX, currentSettings.EditorLevelScaleY));
             Vector2[] points = new Vector2[4];
             points[0] = new Vector2(0f, 0f);
-            points[1] = new Vector2(0f, currentSettings.EditorLevelScaleY);
-            points[2] = new Vector2(currentSettings.EditorLevelScaleX, currentSettings.EditorLevelScaleY);
-            points[3] = new Vector2(currentSettings.EditorLevelScaleX, 0f);
+            points[1] = new Vector2(0f, level.ScaleY);
+            points[2] = new Vector2(level.ScaleX, level.ScaleY);
+            points[3] = new Vector2(level.ScaleX, 0f);
 
             _cameraConfinementCollider.pathCount = 1;
             _cameraConfinementCollider.offset = Vector2.zero;
