1c5e1ea5c7b89bd23f77e22ff98c7e6e0ff24264
Bolrik <62060123+Bolrik@users.noreply.github.com>
2023-10-28 00:09:12 +0200

    'Wave-Dashing' beature

--- Assets/_Project/Scripts/Player/PlayerController/PlayerController_Move.cs
+++ Assets/_Project/Scripts/Player/PlayerController/PlayerController_Move.cs
@@ -9,6 +9,9 @@ namespace Player
         [SerializeField, Tooltip("Raising this value increases collision accuracy at the cost of performance.")]
         private int _freeColliderIterations = 10;
 
+        [field: SerializeField] private float FreeColliderUnstuckDistance { get; set; } = .1f;
+
+
         private Vector3 _velocity;
         private Vector3 _rawMovement;
         private Vector3 Move { get; set; }
@@ -70,17 +73,17 @@ namespace Player
             Vector3 currentPosition = this.transform.position + this._characterBounds.center;
             Vector3 delta = Vector3.zero;
 
-            if (this.IsOnPlatform)
+            // if (this.IsOnPlatform)
             {
                 delta = currentPosition - this.LastValidPosition;
             }
 
             this._rawMovement = new Vector3(this._currentHorizontalSpeed, this._currentVerticalSpeed); // Used externally
             Vector3 move = this._rawMovement * Time.deltaTime + delta;
-            Vector3 furthestPoint = currentPosition + move;
+            Vector3 targetPoint = currentPosition + move;
 
             // check furthest movement. If nothing hit, move and don't do extra checks
-            Collider2D hit = Physics2D.OverlapBox(furthestPoint, this._characterBounds.size, 0, this._groundLayer);
+            Collider2D hit = Physics2D.OverlapBox(targetPoint, this._characterBounds.size, 0, this._groundLayer);
             if (!hit)
             {
                 this.transform.position += move - delta;
@@ -90,29 +93,54 @@ namespace Player
 
             // We are moving
             // otherwise increment away from current pos; see what closest position we can move to
-            Vector3 positionToMoveTo = currentPosition - delta;
-            for (int i = 1; i < this._freeColliderIterations; i++)
+            if (!this.MoveCharacter(currentPosition, delta, targetPoint))
             {
-                // increment to check all but furthestPoint - we did that already
-                float t = (float)i / this._freeColliderIterations;
-                Vector2 posToTry = Vector2.Lerp(currentPosition, furthestPoint, t) - (Vector2)delta;
-
-                if (Physics2D.OverlapBox(posToTry, this._characterBounds.size, 0, this._groundLayer))
+                Debug.Log("Emergency Resolve!");
+                Vector3[] unstuckPoints = new Vector3[]
                 {
-                    // We have a collision, set the previous point!
+                    this.LastValidPosition + new Vector3(move.x, Mathf.Sign(move.y) * -this.FreeColliderUnstuckDistance), // Move Horizontally first
+                    this.LastValidPosition + new Vector3(Mathf.Sign(move.x) * -this.FreeColliderUnstuckDistance, move.y) // And Vertically second
+                };
 
-                    if (i > 1)
-                        positionToMoveTo -= delta;
-
-                    this.LastValidPosition = this.transform.position = positionToMoveTo;
-
-                    return;
+                for (int i = 0; i < unstuckPoints.Length; i++)
+                {
+                    if (this.MoveCharacter(currentPosition, delta, unstuckPoints[i]))
+                        break;
                 }
+            }
+        }
 
-                // No Collision, keep going!
-                positionToMoveTo = posToTry;
+        private bool MoveCharacter(Vector3 currentPosition, Vector3 delta, Vector3 furthestPoint)
+        {
+            Vector3 positionToMoveTo = currentPosition - delta;
+            try
+            {
+                for (int i = 1; i < this._freeColliderIterations; i++)
+                {
+                    // increment to check all but furthestPoint - we did that already
+                    float t = (float)i / this._freeColliderIterations;
+                    Vector2 posToTry = Vector2.Lerp(currentPosition, furthestPoint, t) - (Vector2)delta;
+
+                    if (Physics2D.OverlapBox(posToTry, this._characterBounds.size, 0, this._groundLayer))
+                    {
+                        // We have a collision, set the previous point!
+
+                        if (i > 1)
+                            positionToMoveTo -= delta;
+
+                        return i > 1;
+                    }
+
+                    // No Collision, keep going!
+                    positionToMoveTo = posToTry;
+                }
+            }
+            finally
+            {
+                this.LastValidPosition = this.transform.position = positionToMoveTo;
             }
 
+            return true;
         }
 
         private void HandleSpriteDirection()
