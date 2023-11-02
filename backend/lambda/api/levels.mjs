0473c9c869f459881e630de695f7b9c79a0e0e1f
Nathaniel Morihara <nathanielmorihara@gmail.com>
2023-11-02 21:38:06 +0000

    Backend: Get Levels Sort Options

--- backend/lambda/api/levels.mjs
+++ backend/lambda/api/levels.mjs
@@ -1,12 +1,70 @@
-import { extractLevelId } from "../utils.mjs";
+import { LevelsSortOptions } from "../db/levels.mjs";
+import { BadRequestException, asBool, extractLevelId } from "../utils.mjs";
+
+const defaultPageLimit = 10;
 
 export class LevelsApi {
     constructor(levelsDbClient) {
         this.levelsDbClient = levelsDbClient;
     }
     
-    async getPagedLevels(pageLimit, cursor) {
-        // TODO
+    async getPagedLevels(
+        requestSortOption, 
+        requestSortAsc, 
+        requestLimit, 
+        requestCursor, 
+        requestUseDrafts,
+    ) {
+        var sortOption = LevelsSortOptions.UPDATED_AT;
+        if (requestSortOption !== undefined) {
+            if (!LevelsSortOptions.isValid(requestSortOption)) throw new BadRequestException(`'sort-option' must be one of: ${LevelsSortOptions.getAllValidValues()}`);
+            sortOption = requestSortOption;
+        }
+
+        var sortAsc = false;
+        if (requestSortAsc !== undefined) {
+            sortAsc = asBool(requestSortAsc);
+            if (sortAsc === undefined) throw new BadRequestException(`'sort-asc' must be either 'true' or 'false'`);
+        }
+
+        var pageLimit = defaultPageLimit;
+        if (requestLimit !== undefined) {
+            pageLimit = parseInt(requestLimit);
+            if (isNaN(pageLimit)) throw new BadRequestException(`'limit' must be a number.`);
+        }
+
+        var useDrafts = false;
+        if (requestUseDrafts !== undefined) {
+            useDrafts = asBool(requestUseDrafts);
+            if (useDrafts === undefined) throw new BadRequestException(`'draft' must be either 'true' or 'false'`);
+        }
+
+        var queryResponse = await this.levelsDbClient.getPagedLevels(
+            sortOption,
+            sortAsc,
+            pageLimit, 
+            requestCursor,
+            useDrafts);
+    
+        var dbLevels = queryResponse.levels;
+        var responseLevels = [];
+        for (let i = 0; i < dbLevels.length; i++) {
+            var dbLevel = dbLevels[i];
+
+            // TODO Validation
+
+            responseLevels.push(asApiLevel(dbLevel));
+        }
+
+        var response = {
+            levels: responseLevels
+        }
+
+        if (queryResponse.cursor) {
+            response.cursor = extractLevelId(queryResponse.cursor.pk);
+        }
+
+        return response
     }
 
     async getLevel(levelId) {
@@ -14,21 +72,39 @@ export class LevelsApi {
 
         // TODO Validation of queried response
 
-        // TODO Refactor into a separate function
-        var id = extractLevelId(dbLevel.pk);
-
-        // TODO Include avg and total ratings & scores
-        return {
-            "id": id,
-            "name": dbLevel.levelName,
-            "creator": {
-                "id": dbLevel.levelCreatorId,
-                "name": dbLevel.levelCreatorName
-            },
-            "status": dbLevel.levelStatus,
-            "createdAt": dbLevel.levelCreatedAt,
-            "updatedAt": dbLevel.levelUpdatedAt,
-            "data": dbLevel.levelData
-        }
+        return asApiLevel(dbLevel);
     }
+}
+
+function asApiLevel(dbLevel) {
+    var id = extractLevelId(dbLevel.pk);
+
+    var apiLevel = {
+        "id": id,
+        "name": dbLevel.levelName,
+        "creator": {
+            "id": dbLevel.levelCreatorId,
+            "name": dbLevel.levelCreatorName
+        },
+        "status": dbLevel.levelStatus,
+        "createdAt": dbLevel.levelCreatedAt,
+        "updatedAt": dbLevel.levelUpdatedAt,
+        "totalRatings": dbLevel.levelTotalRatings ?? 0,
+        "totalScores": dbLevel.levelTotalScores ?? 0,
+    };
+
+    if (dbLevel.levelData) {
+        apiLevel.data = dbLevel.levelData;
+    }
+    if (dbLevel.version) {
+        apiLevel.version = dbLevel.version;
+    }
+    if (dbLevel.levelAvgScore) {
+        apiLevel.avgScore = dbLevel.levelAvgScore;
+    }
+    if (dbLevel.levelAvgRating) {
+        apiLevel.avgRating = dbLevel.levelAvgRating;
+    }
+
+    return apiLevel
 }
