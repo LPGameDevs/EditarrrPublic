1d7c92bd680856b3e5eaa3f8e11c38b63c076bdd
Nathaniel Morihara <nathanielmorihara@gmail.com>
2023-11-08 20:12:04 +0000

    Backend: Get Scores for Level - Limit, Sorting, Cursors, and DeDupe Options

--- backend/lambda/api/scores.test.mjs
+++ backend/lambda/api/scores.test.mjs
@@ -7,6 +7,7 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { ScoresApi } from './scores.mjs';
 import { LevelsDbClient } from '../db/levels.mjs';
 import { ScoresDbClient } from '../db/scores.mjs';
+import { expect } from 'chai';
 
 var dynamoDbClient;
 var ddbClientSendStub;
@@ -14,8 +15,10 @@ var scoresDbClient;
 var levelsDbClient;
 var scoresApi;
 
-var tableNameScores = "editarrr-score-storage"
-var tableNameLevels = "editarrr-level-storage"
+var tableNameScores = "editarrr-score-storage";
+var tableNameLevels = "editarrr-level-storage";
+
+var indexLevelNameScore = "scoreLevelName-score-index";
 
 describe('PostScore', function () {
     
@@ -58,6 +61,7 @@ describe('PostScore', function () {
             IndexName: "scoreLevelName-score-index",
             // TODO can we do a subset of attributes?
             Select: "ALL_PROJECTED_ATTRIBUTES",
+            ScanIndexForward: true,
             KeyConditionExpression: "pk = :levelId",
             ExpressionAttributeValues: {
                 ":levelId": `LEVEL#${levelId}`,
@@ -100,5 +104,406 @@ describe('PostScore', function () {
               ":totalScores": 2,
             },
         }));
+    });
+});
+
+describe('GetPagedScores', function() {
+    beforeEach(function () {
+        dynamoDbClient = new DynamoDBDocumentClient(new DynamoDBClient({}));
+        ddbClientSendStub = stub(dynamoDbClient, 'send');
+        scoresDbClient = new ScoresDbClient(dynamoDbClient);
+        levelsDbClient = new LevelsDbClient(dynamoDbClient);
+        scoresApi = new ScoresApi(scoresDbClient, levelsDbClient);
+    });
+
+    it('should fetch the first page of levels sorted by lowest score first & limited to 10 by default.', async function() {
+        // Arrange
+        var levelId = "5246cf90-7f7f-4074-aea1-ba543d27ed63";
+        var sortOptionQueryParam = undefined;
+        var sortAscQueryParam = undefined;
+        var limitQueryParam = undefined;
+        var cursorQueryParam = undefined;
+        var dedupeByUser = undefined;
+
+        ddbClientSendStub.withArgs(match.has("input", {
+            TableName: tableNameScores,
+            IndexName: indexLevelNameScore,
+            Select: "ALL_PROJECTED_ATTRIBUTES",
+            Limit: 10,
+            ScanIndexForward: true,
+            KeyConditionExpression: "pk = :levelId",
+            ExpressionAttributeValues: {
+                ":levelId": "LEVEL#" + levelId
+            }
+        })).returns({
+            "Items":[
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "score": "0.6215752",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698964301163,
+                    "scoreCreatorId": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                    "scoreCreatorName": "Calm Pewter",
+                },
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "score": "1.0",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698938196593,
+                    "scoreCreatorId": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                    "scoreCreatorName": "Murphys dad",
+                },
+            ] 
+        });
+
+        // Act
+        var firstPageOfScores = await scoresApi.getPagedScores(
+            levelId,
+            sortOptionQueryParam,
+            sortAscQueryParam,
+            limitQueryParam,
+            cursorQueryParam,
+            dedupeByUser,
+        );
+
+        // Assert
+        expect(firstPageOfScores).to.deep.equal({
+            "scores": [
+                {
+                    "scoreId": "52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "0.6215752",
+                    "code": "29995",
+                    "creator": {
+                        "id": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                        "name": "Calm Pewter"
+                    },
+                    "submittedAt": 1698964301163
+                },
+                {
+                    "scoreId": "6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "1.0",
+                    "code": "29995",
+                    "creator": {
+                        "id": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                        "name": "Murphys dad"
+                    },
+                    "submittedAt": 1698938196593
+                },
+            ]
+        });
+    });
+
+    it('should fetch the first page of levels sorted by highest score first if the query option is provided.', async function() {
+        // Arrange
+        var levelId = "5246cf90-7f7f-4074-aea1-ba543d27ed63";
+        var sortOptionQueryParam = undefined;
+        var sortAscQueryParam = "false";
+        var limitQueryParam = undefined;
+        var cursorQueryParam = undefined;
+        var dedupeByUser = undefined;
+
+        ddbClientSendStub.withArgs(match.has("input", {
+            TableName: tableNameScores,
+            IndexName: indexLevelNameScore,
+            Select: "ALL_PROJECTED_ATTRIBUTES",
+            Limit: 10,
+            ScanIndexForward: false,
+            KeyConditionExpression: "pk = :levelId",
+            ExpressionAttributeValues: {
+                ":levelId": "LEVEL#" + levelId
+            }
+        })).returns({
+            "Items":[
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "score": "1.0",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698938196593,
+                    "scoreCreatorId": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                    "scoreCreatorName": "Murphys dad",
+                },
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "score": "0.6215752",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698964301163,
+                    "scoreCreatorId": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                    "scoreCreatorName": "Calm Pewter",
+                },
+            ] 
+        });
+
+        // Act
+        var firstPageOfScores = await scoresApi.getPagedScores(
+            levelId,
+            sortOptionQueryParam,
+            sortAscQueryParam,
+            limitQueryParam,
+            cursorQueryParam,
+            dedupeByUser,
+        );
+
+        // Assert
+        expect(firstPageOfScores).to.deep.equal({
+            "scores": [
+                {
+                    "scoreId": "6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "1.0",
+                    "code": "29995",
+                    "creator": {
+                        "id": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                        "name": "Murphys dad"
+                    },
+                    "submittedAt": 1698938196593
+                },
+                {
+                    "scoreId": "52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "0.6215752",
+                    "code": "29995",
+                    "creator": {
+                        "id": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                        "name": "Calm Pewter"
+                    },
+                    "submittedAt": 1698964301163
+                },
+            ]
+        });
+    });
+
+    it('should fetch the first page of levels and return a cursor if there are more.', async function() {
+        // Arrange
+        var levelId = "5246cf90-7f7f-4074-aea1-ba543d27ed63";
+        var sortOptionQueryParam = undefined;
+        var sortAscQueryParam = undefined;
+        var limitQueryParam = "1";
+        var cursorQueryParam = undefined;
+        var dedupeByUser = undefined;
+
+        ddbClientSendStub.withArgs(match.has("input", {
+            TableName: tableNameScores,
+            IndexName: indexLevelNameScore,
+            Select: "ALL_PROJECTED_ATTRIBUTES",
+            Limit: 1,
+            ScanIndexForward: true,
+            KeyConditionExpression: "pk = :levelId",
+            ExpressionAttributeValues: {
+                ":levelId": "LEVEL#" + levelId
+            }
+        })).returns({
+            "Items":[
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "score": "0.6215752",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698964301163,
+                    "scoreCreatorId": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                    "scoreCreatorName": "Calm Pewter",
+                },
+            ],
+            "LastEvaluatedKey": {
+                "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                "score": "0.6215752",
+            },
+        });
+
+        // Act
+        var firstPageOfScores = await scoresApi.getPagedScores(
+            levelId,
+            sortOptionQueryParam,
+            sortAscQueryParam,
+            limitQueryParam,
+            cursorQueryParam,
+            dedupeByUser,
+        );
+
+        // Assert
+        expect(firstPageOfScores).to.deep.equal({
+            "scores": [
+                {
+                    "scoreId": "52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "0.6215752",
+                    "code": "29995",
+                    "creator": {
+                        "id": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                        "name": "Calm Pewter"
+                    },
+                    "submittedAt": 1698964301163
+                },
+            ],
+            "cursor": "52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+        });
+    });
+
+    it('should fetch the second page of levels if a cursor is provided.', async function() {
+        // Arrange
+        var levelId = "5246cf90-7f7f-4074-aea1-ba543d27ed63";
+        var sortOptionQueryParam = undefined;
+        var sortAscQueryParam = undefined;
+        var limitQueryParam = undefined;
+        var cursorQueryParam = "52dff6ed-eb42-4ec1-81d6-d36db8d048d6";
+        var dedupeByUser = undefined;
+
+        ddbClientSendStub.withArgs(match.has("input", {
+            TableName: tableNameScores,
+            Key: {
+                pk: `SCORE#${cursorQueryParam}`,
+                sk: `SCORE#${cursorQueryParam}`,
+            }
+        })).returns({
+            "Item": {
+                "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                "score": "0.6215752",
+                "scoreLevelName": "29995",
+                "scoreSubmittedAt": 1698964301163,
+                "scoreCreatorId": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                "scoreCreatorName": "Calm Pewter",
+            },
+        });
+
+        ddbClientSendStub.withArgs(match.has("input", {
+            TableName: tableNameScores,
+            IndexName: indexLevelNameScore,
+            Select: "ALL_PROJECTED_ATTRIBUTES",
+            Limit: 10,
+            ScanIndexForward: true,
+            KeyConditionExpression: "pk = :levelId",
+            ExpressionAttributeValues: {
+                ":levelId": "LEVEL#" + levelId
+            },
+            ExclusiveStartKey: {
+                "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                "score": "0.6215752",
+            },
+        })).returns({
+            "Items":[
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "score": "1.0",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698938196593,
+                    "scoreCreatorId": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                    "scoreCreatorName": "Murphys dad",
+                },
+            ] 
+        });
+
+        // Act
+        var secondPageOfScores = await scoresApi.getPagedScores(
+            levelId,
+            sortOptionQueryParam,
+            sortAscQueryParam,
+            limitQueryParam,
+            cursorQueryParam,
+            dedupeByUser,
+        );
+
+        // Assert
+        expect(secondPageOfScores).to.deep.equal({
+            "scores": [
+                {
+                    "scoreId": "6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "1.0",
+                    "code": "29995",
+                    "creator": {
+                        "id": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                        "name": "Murphys dad"
+                    },
+                    "submittedAt": 1698938196593
+                },
+            ],
+        });
+    });
+
+    it('should de-dupe by username if requested, returning only the top score (based on the sort order).', async function() {
+        // Arrange
+        var levelId = "5246cf90-7f7f-4074-aea1-ba543d27ed63";
+        var sortOptionQueryParam = undefined;
+        var sortAscQueryParam = undefined;
+        var limitQueryParam = "1";
+        var cursorQueryParam = undefined;
+        var dedupeByUser = "true";
+
+        ddbClientSendStub.withArgs(match.has("input", {
+            TableName: tableNameScores,
+            IndexName: indexLevelNameScore,
+            Select: "ALL_PROJECTED_ATTRIBUTES",
+            ScanIndexForward: true,
+            KeyConditionExpression: "pk = :levelId",
+            ExpressionAttributeValues: {
+                ":levelId": "LEVEL#" + levelId
+            }
+        })).returns({
+            "Items":[
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "score": "0.6215752",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698964301163,
+                    "scoreCreatorId": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                    "scoreCreatorName": "Calm Pewter",
+                },
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "score": "1.0",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698938196593,
+                    "scoreCreatorId": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                    "scoreCreatorName": "Murphys dad",
+                },
+                {
+                    "pk": "LEVEL#5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "sk": "SCORE#6580188b-eec0-44a9-9e56-2de2e5b9a937",
+                    "score": "1.0",
+                    "scoreLevelName": "29995",
+                    "scoreSubmittedAt": 1698938196593,
+                    "scoreCreatorId": "2cbe4992-950e-43bc-9fec-4e6138b5ce74",
+                    "scoreCreatorName": "Murphys dad",
+                },
+            ] 
+        });
+
+        // Act
+        var firstPageOfScores = await scoresApi.getPagedScores(
+            levelId,
+            sortOptionQueryParam,
+            sortAscQueryParam,
+            limitQueryParam,
+            cursorQueryParam,
+            dedupeByUser,
+        );
+
+        // Assert
+        expect(firstPageOfScores).to.deep.equal({
+            "scores": [
+                {
+                    "scoreId": "52dff6ed-eb42-4ec1-81d6-d36db8d048d6",
+                    "levelId": "5246cf90-7f7f-4074-aea1-ba543d27ed63",
+                    "score": "0.6215752",
+                    "code": "29995",
+                    "creator": {
+                        "id": "e0f86b03-7a0f-45d8-a66d-322c4e1196e5",
+                        "name": "Calm Pewter"
+                    },
+                    "submittedAt": 1698964301163
+                },
+            ]
+        });
     });
 });
--- backend/requests/scores/get.sh
+++ backend/requests/scores/get.sh
@@ -8,4 +8,18 @@ if [ $# -eq 1 ]; then
   LEVEL_ID=$1
 fi
 
+echo "Getting the first 10 top (lowest) scores"
 curl ${URL}/${LEVEL_ID}/scores | python3 -m json.tool
+echo
+
+echo "Getting the first 5 top (lowest) scores"
+curl ${URL}/${LEVEL_ID}/scores?'limit=5' | python3 -m json.tool
+echo
+
+echo "Getting the lowest (highest value) 10 scores"
+curl ${URL}/${LEVEL_ID}/scores?'sort-asc=false' | python3 -m json.tool
+echo
+
+echo "De-dupe scores for users (if there are any)"
+curl ${URL}/${LEVEL_ID}/scores?'de-dupe-by-user=true' | python3 -m json.tool
+echo
