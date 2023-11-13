ebcb32c6164bb2376c7ba46fb5ad07ac301deb5b
Julian Schumacher <hello@calmpewter.games>
2023-11-13 16:37:15 +0100

    Credits section up and running, target fps setting updated

---
+++ Assets/_Project/Scripts/UI/CreditsSection.cs
@@ -0,0 +1,19 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using Unity.VisualScripting;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+
+//public class CreditsSection : MonoBehaviour, IPointerClickHandler
+//{
+//    const string TREASURE_HUNTERS_URL = "https://pixelfrog-assets.itch.io/treasure-hunters";
+//    const string RESTCLIENT_URL = "https://github.com/proyecto26/RestClient";
+//    const string MMFEEDBACKS_URL = "https://github.com/reunono/MMFeedbacksPublic";
+    
+//    public void OnPointerClick(PointerEventData eventData)
+//    {
+//        Vector3 mousePosition = new Vector3(eventData.position.x, eventData.position.y, 0);
+//        int linkID = TMP_TextUtilities.FindIntersectingLink();
+//    }
+//}
